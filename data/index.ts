8f0dbd0add7688fc3fa06c80cfcb1c2661926de1
Le Quoc <philippe.lequoc@gmail.com>
2024-06-01 22:08:12 +0200

    add icon image and grid aboutme section

---
+++ data/index.ts
@@ -0,0 +1,224 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+
+// export const gridItems = [
+//     {
+//         id: 1,
+//         title: "Title 1",
+//         description: "Description 1",
+//         className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+//         imgClassName: "w-full h-full",
+//         titleClassName: "justify-end",
+//         img: "/b1.svg",
+//         spareImg: "",
+//     },
+//     {
+//         id: 2,
+//         title: "Title 2",
+//         description: "Description 2",
+//         className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+//         imgClassName: "",
+//         titleClassName: "justify-start",
+//         img: "",
+//         spareImg: "",
+//     },
+//     {
+//         id: 3,
+//         title: "Title 3",
+//         description: "Description 3",
+//         className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+//         imgClassName: "",
+//         titleClassName: "justify-center",
+//         img: "",
+//         spareImg: "",
+//     },
+//     {
+//         id: 4,
+//         title: "Title 4",
+//         description: "Description 4",
+//         className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+//         imgClassName: "",
+//         titleClassName: "justify-start",
+//         img: "/grid.svg",
+//         spareImg: "/b4.svg",
+//     },
+//     {
+//         id: 5,
+//         title: "Title 5",
+//         description: "Description 5",
+//         className: "md:col-span-3 md:row-span-2",
+//         imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+//         titleClassName: "justify-center md:justify-start lg:justify-center",
+//         img: "/b5.svg",
+//         spareImg: "/grid.svg",
+//     },
+//     {
+//         id: 6,
+//         title: "Title 6",
+//         description: "Description 6",
+//         className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+//         imgClassName: "",
+//         titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+//         img: "",
+//         spareImg: "",
+//     },
+// ]
+
+export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+
+export const projects = [
+    {
+        id: 1,
+        title: "title 1",
+        des: "description 1",
+        img: "",
+        iconLists: [],
+        link: "",
+      },
+      {
+        id: 2,
+        title: "title 2",
+        des: "description 2",
+        img: "",
+        iconLists: [""],
+        link: "",
+      },
+]
+
+export const testimonials = [
+    {
+        quote: "quote 1",
+        name: "name 1",
+        title: "title 1",
+    },
+    {
+        quote: "quote 2",
+        name: "name 2",
+        title: "title 2",
+    },
+    {
+        quote: "quote 3",
+        name: "name 3",
+        title: "title 3",
+    },
+]
+
+export const companies = [
+    {
+        id: 1,
+        name: "company 1",
+        img: "",
+        nameImg: "",
+    },
+    {
+        id: 2,
+        name: "company 2",
+        img: "",
+        nameImg: "",
+    },
+]
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "title 1",
+        desc: "description 1",
+        className: "md:col-span-2",
+        thumbnail: "",
+    },
+    {
+        id: 2,
+        title: "title 2",
+        desc: "description 2",
+        className: "md:col-span-2",
+        thumbnail: "",
+    },
+    {
+        id: 3,
+        title: "title 3",
+        desc: "description 3",
+        className: "md:col-span-2",
+        thumbnail: "",
+    },
+]
+
+export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
