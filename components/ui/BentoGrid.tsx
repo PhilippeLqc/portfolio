f87efe0996611f91dde542c832e00c65c781afd9
Le Quoc <philippe.lequoc@gmail.com>
2024-06-02 12:19:36 +0200

    finalisation grid section, add magicbutton to copy email adress, globe acertenity, work on css responsive, add confetti animation

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./BackgroundGradientAnimation";
+import { GridGlobe } from "./GridGlobe";
+import Lottie from "react-lottie";
+import { useState } from "react";
+import animationData from "@/data/confetti.json";
+import MagicButton from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
+import Image from "next/image";
 
 export const BentoGrid = ({
   className,
@@ -39,6 +48,12 @@ export const BentoGridItem = ({
   imgClassName?: string;
   titleClassName?: string;
 }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText("pro@philippelequoc.com");
+    setCopied(true);
+  };
   return (
     <div
       className={cn(
@@ -54,10 +69,12 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
-            <img
+            <Image
               src={img}
               alt={img}
-              className={(cn(imgClassName), "object-cover, object-center")}
+              className={cn(imgClassName, "object-cover object-center")}
+              width={100}
+              height={100}
             />
           )}
         </div>
@@ -67,10 +84,12 @@ export const BentoGridItem = ({
           }`}
         >
           {spareImg && (
-            <img
+            <Image
               src={spareImg}
               alt={spareImg}
-              className={"object-cover, object-center w-full h-full"}
+              className="object-cover, object-center w-full h-full"
+              width={100}
+              height={100}
             />
           )}
         </div>
@@ -82,7 +101,7 @@ export const BentoGridItem = ({
         <div
           className={cn(
             titleClassName,
-            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-5"
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
           )}
         >
           <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
@@ -90,12 +109,71 @@ export const BentoGridItem = ({
           </div>
           <div
             className={cn(
-              "font-sans font-bold text-lg lg:text-3xl max-w-96 z-10",
+              "font-sans font-bold text-lg lg:text-xl max-w-96 z-10",
               titleClassName
             )}
           >
             {title}
           </div>
+
+          {id === 2 && <GridGlobe />}
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-1 w-fit absolute -right-3 lg:right-4">
+              <div className="flex flex-col gap-3 lg:gap-2">
+                {["Angular", "Java", "Spring", "mySQL"].map((tech) => (
+                  <span
+                    className="lg:py-4 lg:px-4 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    key={tech}
+                  >
+                    {tech}
+                  </span>
+                ))}
+                <span
+                  className="lg:py-4 lg:px-4 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                />
+              </div>
+              <div className="flex flex-col gap-3 lg:gap-2">
+                <span
+                  className="lg:py-4 lg:px-4 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                />
+                {["React.js", "Next.js", "Tailwind", "MongoDB"].map((tech) => (
+                  <span
+                    className="lg:py-4 lg:px-4 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    key={tech}
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0`}>
+                <Lottie
+                  options={{
+                    loop: copied,
+                    autoplay: copied,
+                    animationData: animationData,
+                    rendererSettings: {
+                      preserveAspectRatio: "xMidYMid slice",
+                    },
+                  }}
+                />
+              </div>
+              <MagicButton
+                title={copied ? "Email copied" : "Copy my Email"}
+                icon={<IoCopyOutline />}
+                position="left"
+                otherClasses="!bg-[#161a31]"
+                handleClick={handleCopy}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
--- data/index.ts
+++ data/index.ts
@@ -71,9 +71,9 @@ export const navItems = [
 export const gridItems = [
     {
       id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
+      title: "I prioritize user-centric coding, ensuring each feature directly meets user needs and enhances their experience.",
       description: "",
-      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh] md:min-h-[40vh]",
       imgClassName: "w-full h-full",
       titleClassName: "justify-end",
       img: "/b1.svg",
@@ -101,7 +101,7 @@ export const gridItems = [
     },
     {
       id: 4,
-      title: "Tech enthusiast with a passion for development.",
+      title: "Tech enthusiast with a passion for development",
       description: "",
       className: "lg:col-span-2 md:col-span-3 md:row-span-1",
       imgClassName: "",
@@ -112,10 +112,10 @@ export const gridItems = [
   
     {
       id: 5,
-      title: "Currently building a JS Animation library",
-      description: "The Inside Scoop",
-      className: "md:col-span-3 md:row-span-2",
-      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      title: "Currently building a finance management dashboard",
+      description: "SpendSmart",
+      className: "relative md:col-span-3 md:row-span-2",
+      imgClassName: "absolute bottom-0 right-0 md:w-96 w-60",
       titleClassName: "justify-center md:justify-start lg:justify-center",
       img: "/b5.svg",
       spareImg: "/grid.svg",
@@ -124,7 +124,7 @@ export const gridItems = [
       id: 6,
       title: "Do you want to start a project together?",
       description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1 md:max-h-[20vh]",
       imgClassName: "",
       titleClassName: "justify-center md:max-w-full max-w-60 text-center",
       img: "",
