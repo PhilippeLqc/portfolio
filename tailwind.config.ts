155c37b328fb119bc8326365dbd9285b143409a0
Le Quoc <philippe.lequoc@gmail.com>
2024-06-09 14:28:04 +0200

    change B1svg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,14 @@ const config = {
       },
     },
     extend: {
+      textShadow: {
+        'sm': '0 1px 2px rgba(0, 0, 0, 0.05)',
+        'md': '0 4px 6px rgba(0, 0, 0, 0.1)',
+        'lg': '0 10px 15px rgba(0, 0, 0, 0.2)',
+        'xl': '0 20px 25px rgba(0, 0, 0, 0.25)',
+        '2xl': '0 25px 50px rgba(0, 0, 0, 0.3)',
+        // Ajoute d'autres ombres personnalisées ici
+      },
       colors: {
         black: {
           DEFAULT: "#000",
